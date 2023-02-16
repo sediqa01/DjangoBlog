0af5fd190b16e34a619201e1e0fc5f41cd4043f3
Sediqa <sadat.hadid2000@gmail.com>
2023-02-16 21:59:53 +0000

    Deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-blog-w.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-blog-w.herokuapp.com', 'localhost']
 
 
 # Application definition
